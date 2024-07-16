724c7c70081bef28d76acb3d6401b0b5945db8f0
BernieMacHub Mac <jakevalleleyjohnson@outlook.com>
2024-07-16 20:17:55 +0000

    Add function structure to js

---
+++ assets/js/index-game-two.js
@@ -0,0 +1,31 @@
+function getPosition() {
+
+}
+
+function movePlayer() {
+
+}
+
+function checkWin() {
+
+}
+
+function startGame() {
+
+}
+
+function endGame() {
+
+}
+
+function rollDive() {
+
+}
+
+function updateScore() {
+
+}
+
+function checkWin() {
+
+}
