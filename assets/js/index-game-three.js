c11945bb354d6afd509e19aa05dd8420c3842167
BernieMacHub Mac <jakevalleleyjohnson@outlook.com>
2024-07-18 23:29:09 +0000

    Add function to show board pieces paths for moving around

--- assets/js/index-game-three.js
+++ assets/js/index-game-three.js
@@ -31,3 +31,38 @@ function coloring() {
     })
 }
 coloring()
+
+function reddish() {
+    document.querySelectorAll('.box').forEach(i1 => {
+        if (i1.style.backgroundColor == 'lightblue') {
+
+            document.querySelectorAll('.box').forEach(i2 => {
+
+                if (i2.style.backgroundColor == 'lightgreen' && i2.innerText.length !== 0) {
+
+
+                    lightgreenText = i2.innerText
+
+                    lightblueText = i1.innerText
+
+                    lightblueColor = ((Array.from(lightblueText)).shift()).toString()
+                    lightgreenColor = ((Array.from(lightgreenText)).shift()).toString()
+
+                    getId = i2.id
+                    arr = Array.from(getId)
+                    arr.shift()
+                    aside = eval(arr.pop())
+                    aup = eval(arr.shift())
+                    a = aside + aup
+
+                    if (a % 2 == 0 && lightblueColor == lightgreenColor) {
+                        i2.style.backgroundColor = 'rgb(199, 182, 165)'
+                    }
+                    if (a % 2 !== 0 && lightblueColor == lightgreenColor) {
+                        i2.style.backgroundColor = 'rgb(112, 47, 49)'
+                    }
+                }
+            })
+        }
+    })
+}
