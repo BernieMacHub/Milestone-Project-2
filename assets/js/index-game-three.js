1b715c3703f809f47eaa0cd13df93f8a4edf6be2
BernieMacHub Mac <jakevalleleyjohnson@outlook.com>
2024-07-18 23:26:16 +0000

    Add function to set colours on the board

--- assets/js/index-game-three.js
+++ assets/js/index-game-three.js
@@ -7,3 +7,27 @@ let tog = 1; // 1 for White's turn, 0 for Black's turn
 const winningMessageElement = document.getElementById('winningMessageThree');
 const restartButton = document.getElementById('restartButtonThree');
 const winningMessageTextElement = document.querySelector('[data-winning-message-text-three]');
+
+//Coloring
+
+function coloring() {
+    const color = document.querySelectorAll('.box')
+
+    color.forEach(color => {
+
+        getId = color.id
+        arr = Array.from(getId)
+        arr.shift()
+        aside = eval(arr.pop())
+        aup = eval(arr.shift())
+        a = aside + aup
+
+        if (a % 2 == 0) {
+            color.style.backgroundColor = 'rgb(199, 182, 165)'
+        }
+        if (a % 2 !== 0) {
+            color.style.backgroundColor = 'rgb(112, 47, 49)'
+        }
+    })
+}
+coloring()
