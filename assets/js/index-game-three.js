52fa3815bcf6ff401af6885e1e97f20438ef3624
BernieMacHub Mac <jakevalleleyjohnson@outlook.com>
2024-07-18 23:25:06 +0000

    .

--- assets/js/index-game-three.js
+++ assets/js/index-game-three.js
@@ -2,3 +2,8 @@ let numOfKings = 0;
 let whiteScore = 0;
 let blackScore = 0;
 let tog = 1; // 1 for White's turn, 0 for Black's turn
+
+// Winning message and restart button elements
+const winningMessageElement = document.getElementById('winningMessageThree');
+const restartButton = document.getElementById('restartButtonThree');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text-three]');
